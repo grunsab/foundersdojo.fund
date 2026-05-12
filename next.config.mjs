afc627f1473f021ad3e0009e21b2415adb209046
Rishi S <rishi@talkdoc.com>
2026-05-11 22:05:10 -0700

    remove static

--- next.config.mjs
+++ next.config.mjs
@@ -4,24 +4,12 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   turbopack: {
-//     root: __dirname
-//   }
-// };
-
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
-const nextconfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+const nextConfig = {
+  reactStrictMode: true,
+  turbopack: {
+    root: __dirname
+  }
 };
 
-module.exports = nextconfig;
+export default nextConfig;
